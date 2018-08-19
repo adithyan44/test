1fcc6bb8f0bd319a1997f27710c9a1ceb560ebad
Adithyan Das2 <adithyanrdas2@gmail.com>
2018-08-19 21:14:38 +0530

    fnctions except add removed

--- functions.js
+++ functions.js
@@ -1,17 +1,4 @@
 var x=88;
 function add(){
 	return 5+2;
-} 
-function divide(){
-	return 7/2;
-} 
-function sub1(){
-	return 3*2;
-} 
-
-function sub2(){
-	return 3-2;// coments added as new
-}
-function sub3(){
-	return 3-2;// coments added as new
 }
